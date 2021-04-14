aa4114bea9254fd59120e753d87c30f2cb1d1dad
cheech <bacmive@sohu.com>
2021-04-14 13:51:15 +0800

    a

---
+++ memory.ml
@@ -0,0 +1,56 @@
+open ToForte
+open Trajectory
+open Tools
+(* memory *)
+
+let data_size = 2
+let addr_size = 2
+let depth = 4
+
+(** main assertion graph *)
+let vertexI = Vertex 0
+let vertexL = vertexI :: [Vertex 1; Vertex 2]
+let edgeL = [Edge (vertexI, (Vertex 1)); Edge ((Vertex 1), (Vertex 1)); Edge ((Vertex 1), (Vertex 2))]
+
+(** actions of assertion graph *)
+let wr 		: expression = IVar (Ident ("wr", Bool))
+let re 		: expression = IVar (Ident ("re", Bool))
+let low 	: expression = Const (BoolC false) 
+let high 	: expression = Const (BoolC true)
+let addrIn 	: expression = IVar (Ident ("addr", Int addr_size))
+let dataIn  : expression = IVar (Ident ("din", Int data_size))
+let dataOut : expression = IVar (Ident ("dout", Int data_size))
+let symbolAddrIn : expression = Const (SymbIntC ("addr", addr_size))
+let symbolDataIn : expression = Const (SymbIntC ("din", data_size))
+
+let write = Eqn (wr, high)
+let read = AndForm (Eqn(wr, low), Eqn (re, high))
+let noWriteRead = AndForm (Eqn (wr, low), Eqn (re, low))
+
+let writeDataAt d a = AndForm (write, AndForm(Eqn (dataIn, d), Eqn (addrIn, a)))
+let readAt a = AndForm (read, Eqn (addrIn, a))
+let lastReadDataIs d = Eqn (dataOut, d)
+
+let antOfMemory aEdge = 
+	let f = nodeToInt (source aEdge) in
+	let t = nodeToInt (sink aEdge) in
+	(
+		if ((f ==0) && (t ==1)) then writeDataAt symbolDataIn symbolAddrIn
+		else if ((f==1) && (t==1)) then noWriteRead
+		else readAt symbolAddrIn
+	)
+
+let consOfMemory aEdge =
+	let f = nodeToInt (source aEdge) in
+	let t = nodeToInt (sink aEdge) in
+	(
+		if ((f ==0) && (t ==1)) then Chaos
+		else if ((f==1) && (t==1)) then Chaos
+		else lastReadDataIs symbolDataIn
+	)
+
+let memoryGsteSpec = Graph (vertexI, vertexL, edgeL, antOfMemory, consOfMemory)
+
+let binNodes = []
+let () =
+	toFL memoryGsteSpec "memory" binNodes
