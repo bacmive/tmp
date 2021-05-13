90bd679e766f0f79ac6a5ef5209e9f1a200aef4a
cheech <bacmive@sohu.com>
2021-05-13 19:23:22 +0800

    a

---
+++ examples/counter/counter.ml
@@ -0,0 +1,55 @@
+open Types
+open ToForte
+open Trajectory
+open Tools
+
+let last = 3
+let data_size = 2
+
+(********************************* gste assertion graph skeleton ******************************)
+let vertexI = Vertex 0
+let vertexL = vertexI::(List.map (fun i -> Vertex i) (upt 1 (last+1)))
+
+let edgeL = 
+	let e1 = Edge (Vertex 0, Vertex 1) in
+	let e2_list = List.map (fun i -> Edge (Vertex i, Vertex (i+1))) (upt 1 last) in
+	let e3 = Edge (Vertex (last+1), Vertex 1) in
+	(e1::e2_list)@[e3]
+
+(********************************* gste assertion formulas ******************************)
+let reset = IVar (Ident ("rst", Bool))
+let dout = IVar (Ident ("dout", Int data_size))
+let low : expression = Const (BoolC false) 
+let high : expression = Const (BoolC true)
+
+let rstFormula = Eqn(reset, high)
+let noRstFormula =  Eqn(reset, low)
+
+let antOfCounter e = 
+	let f = nodeToInt (source e) in
+	(
+		if(f == 0) then rstFormula
+		else noRstFormula
+	)
+
+let consOfCounter e = 
+	let f = nodeToInt (source e) in
+	(
+		if f == 0 then Chaos
+		else Eqn (dout, Const (IntC ((f-1), data_size)))
+	)
+
+let counterGsteSpec = Graph (vertexI , vertexL,  edgeL, antOfCounter, consOfCounter)
+
+
+(********************************* gste tag invariant ******************************)
+let last_var : expression  = IVar (Ident ("last", Int data_size))
+
+let tag (Vertex n) = 
+	if n = 0 then TAGINV ([], [Chaos])
+	else TAGINV ([], [Eqn (last_var, Const( IntC ((n-1), data_size)))])
+	
+(***********************************************************************************)
+let () =
+	toSTEfl "counter_new" counterGsteSpec tag
+	
--- examples/counter/counter.v
+++ examples/counter/counter.v
@@ -1,18 +1,19 @@
 module counter ( clk, rst, dout);
-	parameter MSBD = 1;
-    
+	parameter LAST = 1;
+	parameter MSBD = 2;
+
     input clk, rst;
-    output [MSBD:0] dout;
-    reg [1:0] last;
+    output [MSBD-1:0] dout;
+
+    reg [MSBD-1:0] last;
     
-    always@ (posedge clk) begin
+    always @ (posedge clk) begin
         if (rst)
             last = 0;
         else begin
             last = last + 1;
         end
     end
-	
-	assign dout = last;
-	
+
+    assign dout = last;
 endmodule
